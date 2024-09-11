b8fcfc9e2351640bde70f4e87c6b0555d6885c39
rupanshigoel <149289988+rupanshigoel@users.noreply.github.com>
2024-09-11 13:05:59 +0530

    Create bin.py

---
+++ bin.py
@@ -0,0 +1,20 @@
+from avl import AVLTree
+class Bin:
+    def __init__(self, bin_id, capacity):
+        self.cap=capacity
+        self.id=bin_id
+        self.atree=AVLTree()
+        pass
+
+    def avl_search(self,id):
+        
+    def add_object(self, object):
+        # Implement logic to add an object to this bin
+        self.list.append(object.id)
+        self.cap=self.cap-object.size
+        pass
+
+    def remove_object(self, object_id):
+        # Implement logic to remove an object by ID
+
+        pass
